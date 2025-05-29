80af5639447c6935fd7c5ab084f3987f56553a5b
robotics-franka-gh <robotics+franka-github@labelbox.com>
2025-05-28 21:08:02 -0700

    working

--- ros2_moveit_franka/build/ros2_moveit_franka/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/labelbox/projects/moveit/lbx-Franka-Teach/ros2_moveit_franka/install/ros2_moveit_franka'
